eb0df05646aba51d7c2b042c81ebe4b04f0ae25c
EduNagado <eduardo.nagado@gmail.com>
2025-04-14 13:46:43 -0300

    fix: Finalização do jogo

--- jogoDaVelha.js
+++ jogoDaVelha.js
@@ -1,6 +1,5 @@
-let jogador = "X";
 let jogadorVez = "O";
-let jogadorAtual = "";
+let jogadorAtual = "X"; 
 let quadrado = "";
 let jogadas = 0;
 let jogadasMaximas = 9;
@@ -10,22 +9,22 @@ function exibirJogadorDaVez() {
     exibirJogador.innerHTML = `Jogador da vez: ${jogadorAtual}`;
 }
 
-
 function posicao(index) {
     const cell = document.getElementById(`cell-${index}`);
 
     if (cell.textContent === "") {
+        const jogadorAnterior = jogadorAtual; 
         cell.textContent = jogadorAtual;
-        jogadorAtual = jogadorAtual === "X" ? "O" : "X";
         jogadas++;
 
+        verificarVitoria(jogadorAnterior); 
+
+        jogadorAtual = jogadorAtual === "X" ? "O" : "X";
         exibirJogadorDaVez();
-        verificarVitoria();
     }
-    
 }
 
-function verificarVitoria() {
+function verificarVitoria(jogadorVerificado) {
     const combinacoesVitoria = [
         [0, 1, 2],
         [3, 4, 5],
@@ -36,34 +35,37 @@ function verificarVitoria() {
         [0, 4, 8],
         [2, 4, 6]
     ];
+
     for (let i = 0; i < combinacoesVitoria.length; i++) {
         const [a, b, c] = combinacoesVitoria[i];
         const cellA = document.getElementById(`cell-${a}`);
         const cellB = document.getElementById(`cell-${b}`);
         const cellC = document.getElementById(`cell-${c}`);
 
-        if (cellA.textContent === jogadorAtual && cellB.textContent === jogadorAtual && cellC.textContent === jogadorAtual) {
-            alert(`Jogador ${jogadorAtual} venceu!`);
+        if (
+            cellA.textContent === jogadorVerificado &&
+            cellB.textContent === jogadorVerificado &&
+            cellC.textContent === jogadorVerificado
+        ) {
+            alert(`Jogador ${jogadorVerificado} venceu!`);
             reiniciarJogo();
             return;
         }
     }
+
     if (jogadas === jogadasMaximas) {
         alert("Empate!");
         reiniciarJogo();
     }
-
-
-}  
+  
+}
 
 function reiniciarJogo() {
     for (let i = 0; i < jogadasMaximas; i++) {
         const cell = document.getElementById(`cell-${i}`);
         cell.textContent = "";
     }
+    jogadorAtual = "X"; 
+    jogadas = 0;
     exibirJogadorDaVez();
 }
-
-const exibirJogador = document.getElementById("jogadorDaVez");
-
-
